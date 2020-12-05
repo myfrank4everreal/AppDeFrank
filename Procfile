ca44599cd9ba480f88e21fd23c3edabb2fea2fb3
myfrank4everreal <myfrank4everreal@gmail.com>
2020-12-05 04:29:48 +0400

    added requirement txt files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myapps.wsgi
--- myapps/settings.py
+++ myapps/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+from decouple import config 
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,5 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[ os.path.join(BASE_DIR, 'myapps/static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapps/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
