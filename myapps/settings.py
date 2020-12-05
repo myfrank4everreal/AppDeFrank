3bea049de59850eaec1f51be1f5578aa8cc3ff7d
myfrank4everreal <myfrank4everreal@gmail.com>
2020-12-05 04:38:25 +0400

    whitenoise setttings

--- myapps/settings.py
+++ myapps/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +132,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapps/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
+
